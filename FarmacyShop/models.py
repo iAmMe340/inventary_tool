738dce44f31a2148a7cf6e4bf0f5e36b461f8186
Wichmann Alexander Saenz <wichmann.saenz@outlook.com>
2021-08-20 13:22:08 -0500

    realizado modelos de DB farmacy shop in azus computer

--- FarmacyShop/models.py
+++ FarmacyShop/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Proveedor(models.Model):
+    cod_proveedor = models.CharField(max_length=8, primary_key=True)
+
+
+
+class Product(models.Model):
+    cod_producto=models.CharField(max_length=8 , primary_key=True)
+    proveedor=models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+
+
+class Stock(models.Model):
+    cod_producto=models.ForeignKey(Product, on_delete=models.CASCADE)
+    descripcion=models.CharField(max_length=50, blank=True)
