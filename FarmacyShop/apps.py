8699d43c43106ad00081a8116d7c22896c88d89f
Wichmann Alexander Saenz <wichmann.saenz@outlook.com>
2021-08-20 11:16:26 -0500

    elkin carga modelos de db

---
+++ FarmacyShop/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FarmacyshopConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'FarmacyShop'
---
+++ MantenimientoShop/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MantenimientoshopConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MantenimientoShop'
